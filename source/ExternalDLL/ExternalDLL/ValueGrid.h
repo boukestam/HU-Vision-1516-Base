2376805442ca69ecf095ddce287b6cc5a236f771
scottmackay2 <scottmackay@live.nl>
2016-05-01 19:54:44 +0200

    Improved speed of maskImage() function. Also added implementationPlan and testRaports.

--- source/ExternalDLL/ExternalDLL/ValueGrid.h
+++ source/ExternalDLL/ExternalDLL/ValueGrid.h
@@ -15,6 +15,8 @@ public:
 	void setValue(int x, int y, double value) const;
 	void setValue(int i, double value) const;
 
+	double * getData() const;
+
 	int getSize() const;
 private:
 	double * data;
